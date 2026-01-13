c3d372a6d412bda8e9ab0d2d12fe95df5af100bd
alim395 <atm17871@gmail.com>
2026-01-13 12:00:19 +0300

    Globalized playSFX Function
    playSFX() is now a global function with one simple implementation.
    Also added GlassTap Sound for panel button.

--- scripts/music_panel_button.gd
+++ scripts/music_panel_button.gd
@@ -1,5 +1,7 @@
 extends TextureButton
 
+@export var glassTap : AudioStream
+
 @export var panelVisuals : Array[Control]
 var currentIndex = 0
 
@@ -8,9 +10,12 @@ func _ready() -> void:
 	if not panelVisuals:
 		queue_free()
 	else:
-		for v in panelVisuals:
-			v.visible = false
-		panelVisuals[0].visible = true
+		if not (panelVisuals.size() == 1):
+			for v in panelVisuals:
+				v.visible = false
+			panelVisuals[0].visible = true
+		else:
+			queue_free()
 
 func _on_pressed() -> void:
 	currentIndex += 1
@@ -19,3 +24,4 @@ func _on_pressed() -> void:
 	for v in panelVisuals:
 		v.visible = false
 	panelVisuals[currentIndex].visible = true
+	globalParameters.playSFX(glassTap)
