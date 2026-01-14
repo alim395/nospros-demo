534a2d6e147269674ddd8df9a6ef726947686124
alim395 <atm17871@gmail.com>
2026-01-14 22:05:42 +0300

    Implemented Main Menu Settings
    - Re-enabled Options Screen
    - Added Filter Settings
    - Adjusted UI Containers
    - Fixed Main Scene Bug

---
+++ scripts/setting_screen.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@export var softBlur : ColorRect
+@export var highFidelity : CheckButton
+@export var crtFilter : CheckButton
+@export var crtNode : CRT
+@export var ScreenFilters : Node2D
+#@export var startMenuOptions : OptionButton
+#@export var iconStyleOptions : OptionButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ScreenFilters.visible = true;
+	softBlur.visible = not globalParameters.highFidelity
+	crtNode.visible = globalParameters.crtFilter
+	highFidelity.button_pressed = globalParameters.highFidelity
+	crtFilter.button_pressed = globalParameters.crtFilter
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	softBlur.visible = not toggled_on
+	globalParameters.highFidelity = toggled_on
+	print("SOFT TOGGLED")
+
+func _on_crt_button_toggled(toggled_on: bool) -> void:
+	crtNode.visible = toggled_on
+	globalParameters.crtFilter = toggled_on
+	print("CRT TOGGLED")
