3d8cd51c520f348eda8caff6fe14938c59ff9db6
alim395 <atm17871@gmail.com>
2026-08-05 11:33:30 +0300

    Updated Credits Song

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -5,7 +5,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	MusicManager.play_song.emit(creditsSong, false, false,0.5)
+	MusicManager.play_song.emit(creditsSong, false, false, 0.5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
